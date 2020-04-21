91938f7e834878c60d0346a9bf6ac3aea5d6daa7
Jonathan Lui <jonslui@gmail.com>
2020-04-21 10:15:25 -0400

    Added autosave serialization and incorrect guesses display

--- hangman.rb
+++ hangman.rb
@@ -1,11 +1,13 @@
+require 'yaml'
+
 class Hangman
 
-    attr_accessor :word_to_guess, :word_shown, :guessed_letter
+    attr_accessor :word_to_guess, :word_shown, :guessed_letter, :previously_guessed
 
     def initialize(dictionary_file)
         @word_to_guess = get_word_from_dictionary(dictionary_file)
         @word_shown = []
-        @guessed_letter = ""
+        @incorrect_guesses = []
         @guesses_left = 6
     end
 
@@ -22,14 +24,16 @@ class Hangman
     end  
     
     def player_guess
+        puts "Incorrect guesses: #{@incorrect_guesses.join("")}"
         print "Guess a letter: "
         player_guess = gets.downcase.chomp
+        puts " "
         
         while player_guess.match?(/[A-Z]/i) == false || player_guess.length != 1
             print "Guess a letter: "
             player_guess = gets.downcase.chomp
         end
-    
+        
         player_guess.gsub(/\s+/,"")
     end
     
@@ -39,6 +43,8 @@ class Hangman
            char == letter ? locations << index : ""
         }
         if locations == []
+            @incorrect_guesses << letter
+            puts "Incorrect guess! #{@guesses_left} attempts left!"
             @guesses_left -= 1
         end
 
@@ -55,6 +61,8 @@ class Hangman
 
     def check_for_win
        if @word_shown.include?(" _ ") == false
+        # delete savestate if player wins
+        File.delete('savestate.yml')
         true
        else
         false
@@ -80,28 +88,41 @@ end
 
 
 
-
-
-
-
-
-
 dictionary_file = File.open('5desk.txt').to_a
-save_state = File.open('savestate.txt', 'w')
 
 # initialize game
-currentGame = Hangman.new(dictionary_file)
-puts currentGame.word_to_guess
-currentGame.word_to_guess.length.times { currentGame.word_shown << " _ "}
-currentGame.display(currentGame.word_shown)
+if File.exist?('savestate.yml')
+    print "Do you want to reload your last game? Y/N: "
+    replay = gets.chomp
+end
+
+if replay != nil && replay.downcase == "y"
+    currentGame = YAML.load(File.read('savestate.yml'))
+    currentGame.display(currentGame.word_shown)
+else
+    currentGame = Hangman.new(dictionary_file)
+    puts currentGame.word_to_guess
+    currentGame.word_to_guess.length.times { currentGame.word_shown << " _ "}
+    currentGame.display(currentGame.word_shown)
+end
+
+
 
 
 
 # # gameplay loop
 while currentGame.check_for_win() == false && currentGame.still_have_guesses? == true
-guessed_letter = currentGame.player_guess()
-location_of_guessed_letters = currentGame.check_for_letter(currentGame.word_to_guess, guessed_letter)
-currentGame.update_word_shown(currentGame.word_shown, location_of_guessed_letters, guessed_letter)
-currentGame.display(currentGame.word_shown)
+    guessed_letter = currentGame.player_guess()
+    location_of_guessed_letters = currentGame.check_for_letter(currentGame.word_to_guess, guessed_letter)
+    currentGame.update_word_shown(currentGame.word_shown, location_of_guessed_letters, guessed_letter)
+    currentGame.display(currentGame.word_shown)
+    File.open('savestate.yml','w'){|file| file.write(currentGame.to_yaml)}
 end
+
+# implement save state
+# record 
+    # @word_to_guess = get_word_from_dictionary(dictionary_file)
+    # @word_shown = []
+    # @previously_guessed = []
+    # @guesses_left = 6
 
